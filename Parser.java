c0dc927e2a0ded62992ced005e1c415cf5cf1a14
Oleh <94360435+Muwko@users.noreply.github.com>
2022-08-22 11:39:07 +0300

    Add files via upload

---
+++ Parser.java
@@ -0,0 +1,97 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.net.URL;
+import java.io.IOException;
+
+
+public class Parser {
+
+    private static Document getPage() throws IOException {
+        String url = "https://finance.ua/ru/currency";
+        return Jsoup.parse(new URL(url), 3000);
+    }
+
+    //private void print
+
+    public static void main(String[] args) throws IOException {
+
+        Document page = getPage();
+        //Налічний курс
+        Elements tableCourse = page.select("table[class=b-market-table_currency-cash]");
+        Elements names = tableCourse.select("tr[class=major]");
+        Elements values = tableCourse.select("td[class=c2]");
+
+        Elements namesEuro = tableCourse.select("tr[class=major major-last]");
+        Elements valuesEuro = tableCourse.select("td[class=c2]");
+
+
+        //M - Міняйло
+        Elements tableCourseM = page.select("table[class=b-market-table_currency-order]");
+        Elements namesM = tableCourse.select("tr[class=major]");
+        Elements valuesM = tableCourse.select("td[class=c2]");
+
+        Elements namesEuroM = tableCourse.select("tr[class=major]");
+        Elements valuesEuroM = tableCourse.select("td[class=c2]");
+        int index = 0;
+
+        //МіжБанк
+        Elements tableCourseI = page.select("table[class=b-market-tavle_currency-interbank table-layout-ukrdealing]");
+        Elements namesI = tableCourse.select("tr[class=major]");
+        Elements valuesI = tableCourse.select("td[class=c2]");
+
+
+        Elements namesEuroI = tableCourse.select("tr[class=major]");
+        Elements valuesEuroI = tableCourse.select("td[class=c2]");
+
+
+        for (Element name : names) {
+            String dollarBuy = name.select("td[class=c2]").text();
+            String dollarSell = name.select("td[class=c3]").text();
+            System.out.println("Dollar-Cash:     Purchase      Sale");
+            System.out.println("                 " + dollarBuy + "       " + dollarSell);
+        }
+
+        for (Element name : namesEuro) {
+            String euroBuy = name.select("td[class=c2]").text();
+            String euroSell = name.select("td[class=c3]").text();
+            System.out.println("   ");
+            System.out.println("Euro-Cash:       Purchase      Sale");
+            System.out.println("                 " + euroBuy + "       " + euroSell);
+        }
+
+        for (Element name : namesM) {
+            String dollarBuy = name.select("td[class=c2]").text();
+            String dollarSell = name.select("td[class=c3]").text();
+            System.out.println("Cours Minyajlo");
+            System.out.println("Dollar-Change:   Purchase      Sale");
+            System.out.println("                 " + dollarBuy + "       " + dollarSell);
+        }
+
+        for (Element name : namesEuroM) {
+            String euroBuy = name.select("td[class=c2]").text();
+            String euroSell = name.select("td[class=c3]").text();
+            System.out.println("   ");
+            System.out.println("Euro-Change:     Purchase      Sale");
+            System.out.println("                 " + euroBuy + "       " + euroSell);
+        }
+        for (Element name : namesI) {
+            String dollarBuy = name.select("td[class=c2]").text();
+            String dollarSell = name.select("td[class=c3]").text();
+            System.out.println("Cours Interbank");
+            System.out.println("Dollar-Change:   Purchase      Sale");
+            System.out.println("                 " + dollarBuy + "       " + dollarSell);
+        }
+
+        for (Element name : namesEuroI) {
+            String euroBuy = name.select("td[class=c2]").text();
+            String euroSell = name.select("td[class=c3]").text();
+            System.out.println("   ");
+            System.out.println("Euro-Change:     Purchase      Sale");
+            System.out.println("                 " + euroBuy + "       " + euroSell);
+        }
+
+    }
+    }
